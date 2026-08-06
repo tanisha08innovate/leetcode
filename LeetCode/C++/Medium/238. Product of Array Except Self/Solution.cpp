fb85520e8dc4efa8b9fd6ff25d111532b0aac3be
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-06 22:26:00 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/238. Product of Array Except Self/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>ans(nums.size(), 1);
+        vector<int>prefix(nums.size(), 1);
+        vector<int>suffix(nums.size(), 1);
+        int n = nums.size();
+        prefix[0]=1;
+        suffix[n-1]=1;
+
+        for(int i=1; i<n; i++) {
+         prefix[i] = prefix[i-1] * nums[i-1];
+        }
+        for(int i=n-2; i>=0; i--) {
+            suffix[i]= suffix[i+1]*nums[i+1];
+        }
+        for(int i=0; i<n; i++) {
+            ans[i] = prefix[i]*suffix[i];
+        }
+        
+        return ans;
+    }
+};
