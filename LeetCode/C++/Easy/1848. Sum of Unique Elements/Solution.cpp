5e2e89002b0f3f9613f2e30c6ee85d1dc0126ec1
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-06 22:23:57 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/1848. Sum of Unique Elements/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<n; i++) {
+            int count=0;
+            for(int j=0; j<n; j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+            }
+            if(count == 1) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+};
