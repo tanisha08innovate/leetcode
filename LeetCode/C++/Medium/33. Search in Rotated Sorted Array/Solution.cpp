38b8dbeecb14007602170234c09f0dd68f2e3e14
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-06 22:23:20 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/33. Search in Rotated Sorted Array/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n= nums.size();
+        int st=0;
+        int end=n-1;
+
+        while(st <= end) {
+             int mid = st + (end-st)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            if(nums[st] <= nums[mid]) { //left sorted 
+              if(nums[st] <= target && target <= nums[mid]) { //left sorted ke left half me
+                end= mid-1;
+              } else{
+                st = mid+1; // left sorted ke right half me
+              }
+            } else{
+                if(nums[mid] <= target && target <= nums[end]) {
+                    st = mid+1;
+                } else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
