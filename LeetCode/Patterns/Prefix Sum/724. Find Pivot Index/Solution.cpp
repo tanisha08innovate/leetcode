411a9990339255b4553817c99e82bb0b108b9c90
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-21 18:34:52 +0530

    Add C++ Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/724. Find Pivot Index/Solution.cpp
+++ LeetCode/Patterns/Prefix Sum/724. Find Pivot Index/Solution.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        int lfsum = 0;
-        int rtsum = 0;
-        
-        // Sum elements to the left of i
-        for (int k = 0; k < i; k++) lfsum += nums[k];
-        
-        // Sum elements to the right of i
-        for (int j = i + 1; j < n; j++) rtsum += nums[j];
-        
-        if (rtsum == lfsum) {
-            return i;
+        int leftsum=0;
+        int totalsum= accumulate(nums.begin(), nums.end(), 0);
+       
+        for(int i=0; i<n; i++) {
+            int rightsum= totalsum-leftsum-nums[i];
+          if(leftsum==rightsum) {
+          return i;
         }
+        leftsum+=nums[i];
     }
     return -1;
-        
     }
 };
