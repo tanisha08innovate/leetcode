a1edd0d015a2692cd8966a267bd673b9f4c6c539
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-21 17:53:07 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1168. Duplicate Zeros/Solution.cpp
+++ LeetCode/C++/Easy/1168. Duplicate Zeros/Solution.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         int n = arr.size();
-        int count=0;
+       
         for(int i=0; i<n-1; i++) {
             if(arr[i] == 0) {
-                count++;
+              
                 arr.insert(arr.begin()+i+1, 0);
                 i++;
                 arr.erase(arr.end());
