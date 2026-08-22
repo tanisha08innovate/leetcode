e2936ec9cf11a7eb25a1a9287dc093cb9cb6f7fa
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-22 20:23:04 +0530

    Add C++ Solution to Patterns/Math

---
+++ LeetCode/Patterns/Math/2608. Count the Digits That Divide a Number/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count=0;
+        int orginal=num;
+        while(num>0) {
+            int digit = num%10;
+            if(orginal % digit==0) {
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+    }
+};
