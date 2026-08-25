4452eef5bbf51e5343853316cbdd67827103a73c
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-25 10:48:09 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/26. Remove Duplicates from Sorted Array/Solution.cpp
+++ LeetCode/C++/Easy/26. Remove Duplicates from Sorted Array/Solution.cpp
@@ -3,10 +3,9 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n= nums.size();
         int position=0;
-        int k = nums.size();
         for(int i =0; i<n-1; i++) {
             if(nums[i] != nums[i+1]) {
-                nums[position] = nums[i];
+                nums[position] = nums[i]; //sorted hai duplicates are always next to each other
                 position++;
             }
             }
