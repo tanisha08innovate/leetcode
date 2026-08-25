d6d4ce0b33d2ce461c4bf09080d299ce58010162
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-08-25 11:02:35 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
        int n= nums.size();
-        int count =0;
+       int position=0;
+
        for(int i=0; i<n; i++) {
-        if(nums[i]==0) {
-            count++;
-            nums.erase(nums.begin() + i);
+        if(nums[i] != 0) {
+            nums[position] = nums[i];
+            position++;
         }
        }
-       for(int i=0; i<n; i++) {
-        if(n< i+1+count) {
-        nums.push_back(0);
-       }
+       for(int i=position; i<n; i++) {
+        nums[i]=0;
        }
     }
 };
