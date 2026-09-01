20216000d4e69a3833e80bf9f56a00c0e934e31e
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-01 21:11:17 +0530

    Add Easy C++ Solution for 169. Majority Element from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/169. Majority Element/Solution.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
     int n = nums.size();
-     unordered_map<int, int>m;
-     int first=0;
-     int second=0;
 
     for(int i=0; i<n; i++) {
-       first=nums[i];
-       second=nums[i];
-       if(m.find(second) != m.end()) {
-        m[first]++;
-       } else{
-        m[first]=1;
-       }
+        int count =0;
+        for(int j=0; j<n; j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+        }
+        if(count > (n/2)) {
+           return nums[i];
+        }
     }
-     if(m[first]>n/2) {
-        return first;
-    }
-    return -1; 
+    return -1;
+        
     }
 };
