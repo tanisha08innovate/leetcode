b9a6e6ec280bfb2d05b528fc0e988b652cb9de09
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-03 20:04:20 +0530

    Add Easy C++ Solution for 283. Move Zeroes from LeetCode

--- LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroCount = 0;
-        if(nums[0]==0) {
-             nums.erase(nums.begin());
-           nums.push_back(0);
+       int n= nums.size();
+       int position=0;
+
+       for(int i=0; i<n; i++) {
+        if(nums[i] != 0) {
+            nums[position] = nums[i];
+            position++;
         }
-        for(int i=1; i<nums.size(); i++) {
-            if(nums[i] == 0) {
-           nums.erase(nums.begin()+i);
-           nums.push_back(0);
-                zeroCount++;
-        } else{
-                continue;
-        }
-    }
+       }
+       for(int i=position; i<n; i++) {
+        nums[i]=0;
+       }
     }
 };
