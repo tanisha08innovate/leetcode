651e1eba6ad7846bfefa3f67ecefdf6dd8bdb9c3
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-08 18:46:46 +0530

    Add Medium C++ Solution for 31. Next Permutation from LeetCode

--- LeetCode/Patterns/Two Pointers/31. Next Permutation/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/31. Next Permutation/Solution.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-       int n= a.size();
-       
-        int piv=-1;
-        for(int i=n-2; i>=0; i--) {
-            if(a[i] < a[i+1]) {
-                piv=i;
-                break;
-            }
-        }
-        if(piv==-1) {
-            reverse(a.begin(), a.end());
-            return;
-        }
-        for(int i=n-1; i>piv; i--) {
-          if(a[i]>a[piv]) {
-            swap(a[i], a[piv]);
-            break;
-          }
-        }
-        int i= piv+1;
-        int j=n-1;
-        while(i<=j) {
-            swap(a[i], a[j]);
-            i++;
-            j--;
-        }
+       next_permutation(a.begin(), a.end());
     }
 };
