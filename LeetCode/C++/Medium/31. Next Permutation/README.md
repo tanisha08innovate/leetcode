c16997ac026c0a37688d3eb25bcbdf7f5d331c26
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-08 20:39:54 +0530

    Add Medium C++ Solution for 31. Next Permutation from LeetCode

--- LeetCode/C++/Medium/31. Next Permutation/README.md
+++ LeetCode/C++/Medium/31. Next Permutation/README.md
@@ -8,8 +8,8 @@
 Array, Two Pointers
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 15.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Medium/31. Next Permutation/Solution.cpp
+++ LeetCode/C++/Medium/31. Next Permutation/Solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-       next_permutation(a.begin(), a.end());
+      int n = a.size();
+      
     }
 };
