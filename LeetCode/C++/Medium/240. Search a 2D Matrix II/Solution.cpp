ed311ea80a6399baaeb165c58a87a2f0b9f4dda4
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-11 20:22:07 +0530

    Add Medium C++ Solution for 240. Search a 2D Matrix II from LeetCode

--- LeetCode/C++/Medium/240. Search a 2D Matrix II/Solution.cpp
+++ LeetCode/C++/Medium/240. Search a 2D Matrix II/Solution.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int r= 0;
-        int c= n-1;
+        int c= m-1;
         while(r<m && c>=0){
             if(target==matrix[r][c]) {
                return true;
