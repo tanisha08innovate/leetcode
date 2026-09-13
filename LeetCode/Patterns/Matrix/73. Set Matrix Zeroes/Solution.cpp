03e8ca0a3dffb547b85f525e2ed4d9abc196ce25
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-13 19:25:16 +0530

    Add Medium C++ Solution for 73. Set Matrix Zeroes from LeetCode

--- LeetCode/Patterns/Matrix/73. Set Matrix Zeroes/Solution.cpp
+++ LeetCode/Patterns/Matrix/73. Set Matrix Zeroes/Solution.cpp
@@ -3,24 +3,39 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        int col=1;
 
-        vector<int>row(m,0); //m elements initialised to 0
-        vector<int>col(n,0); // n elements initialised to 0
-
-        for(int i= 0; i<m; i++){
-           for(int j=0; j<n; j++) {
-            if(matrix[i][j]==0) {
-                row[i]=1; //remeber row[i]
-                col[j]=1; ////remeber col[j]
-            }
-           }
-        }
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(row[i]==1 || col[j]==1) {
-                    matrix[i][j]=0;
+                if(matrix[i][j]==0) {
+                    matrix[i][0]=0; //mark the ith row
+                    if(j != 0) {
+                    matrix[0][j]=0; //mark the jth colom when it dosent collide with the row
+                    }else{ //if it collides 
+                     col=0; //use additionl block
+                    }
                 }
             }
+        } 
+        for(int i=m-1; i>0; i--) {
+            for(int j=n-1; j>0; j--) {
+                if(matrix[i][j] != 0) {
+                    //checl for colom and row
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        } 
+        if(matrix[0][0]==0) {
+            for(int j=0; j<n; j++) {
+            matrix[0][j]=0;
+        }      
+    }
+    if(col==0) {
+        for(int i=0; i<m; i++) {
+            matrix[i][0]=0;
         }
+    }
     }
 };
