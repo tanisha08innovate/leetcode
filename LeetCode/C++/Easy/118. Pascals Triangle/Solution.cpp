0d405f2e3089b044dcce7a7b02536bc49526ef19
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-25 11:41:41 +0530

    Add Easy C++ Solution for 118. Pascal's Triangle from LeetCode

--- LeetCode/C++/Easy/118. Pascals Triangle/Solution.cpp
+++ LeetCode/C++/Easy/118. Pascals Triangle/Solution.cpp
@@ -1,26 +1,34 @@
-class Solution {
-public:
-    int nCr(int n, int r) {
-      int ans=1;
-      for(int i=1; i<=r; i++) {
-        ans= ans*(n-i+1);
-        ans= ans/i;
-      }
-      return ans;
 
-}
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> answ;
-        long long n = numRows;
-        for(int row=1; row<=n; row++) {
-            vector<int>temp;
-           
-            for(int col=1; col<=row; col++) {
-                temp.push_back(nCr(row-1, col-1));
-            }
-          
-            answ.push_back(temp);
+   class Solution {
+public:
+
+    vector<int> generateRow(int row) {
+
+        long long ans = 1;
+        vector<int> ansRow;
+
+        ansRow.push_back(1);
+
+        for(int col = 1; col < row; col++) {
+
+            ans = ans * (row - col);
+            ans = ans / col;
+
+            ansRow.push_back(ans);
         }
-        return answ;
+
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> ans;
+
+        for(int i = 1; i <= numRows; i++) {
+
+            ans.push_back(generateRow(i));
+        }
+
+        return ans;
     }
 };
--- LeetCode/Patterns/Dynamic Programming/118. Pascals Triangle/README.md
+++ LeetCode/Patterns/Dynamic Programming/118. Pascals Triangle/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 9.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
