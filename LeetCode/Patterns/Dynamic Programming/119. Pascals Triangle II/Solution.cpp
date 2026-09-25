f0d5c2c2e1fc32d09908d7cb28a1931f769f23a7
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-25 12:01:57 +0530

    Add Easy C++ Solution for 119. Pascal's Triangle II from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/119. Pascals Triangle II/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        int n = rowIndex;
+        int ans=1;
+        vector<int>result;
+        result.push_back(1);
+        for(int i=1; i<=n; i++) {
+           ans= ans*(n-i+1);
+           ans=ans/i;
+           result.push_back(ans);
+        }
+      
+        return result;
+    }
+};
