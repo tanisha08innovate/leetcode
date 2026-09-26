c16df4341cc1dafb2183cb2c252ea3787cf92c93
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-26 12:24:31 +0530

    Add Hard C++ Solution for 493. Reverse Pairs from LeetCode

--- LeetCode/Patterns/Trees/493. Reverse Pairs/Solution.cpp
+++ LeetCode/Patterns/Trees/493. Reverse Pairs/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
-        int count=0;
+        int count=0; 
         for(int i=0; i<nums.size(); i++) {
             for(int j=i+1; j<nums.size(); j++) {
                 if(nums[i]>2*(nums[j])) {
