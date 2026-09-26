be31bdfa2845f7c4bff9ae680e99b7f39c227902
tanisha08innovate <tanisharaghuvanshi2303@gmail.com>
2026-09-26 19:09:55 +0530

    Add Medium C++ Solution for 152. Maximum Product Subarray from LeetCode

--- LeetCode/C++/Medium/152. Maximum Product Subarray/README.md
+++ LeetCode/C++/Medium/152. Maximum Product Subarray/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 0 ms
+- **Memory:** 17.9 MB
 
 ---
 
--- LeetCode/C++/Medium/152. Maximum Product Subarray/Solution.cpp
+++ LeetCode/C++/Medium/152. Maximum Product Subarray/Solution.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
+        int n = nums.size();
+        int currprod=1;
+        int maxprod= INT_MIN;
+
+        for(int i=0; i<n; i++) {
+            currprod *= nums[i];
+            maxprod = max(maxprod, currprod);
+           if(currprod==0) {
+            currprod=1;
+           }
+        }
+        currprod=1;
+        for(int i=n-1; i>=0; i--) {
+            currprod *= nums[i];
+            maxprod= max(maxprod, currprod);
+            if(currprod==0) {
+                currprod=1;
+            }
+        }
+        return maxprod;
     }
 };
